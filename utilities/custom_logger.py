f5798058c2878fb4ddd8cdbb582bb9472fc9041f
prashantFarakate <prashantf18@gmail.com>
2025-01-14 00:01:29 +0530

    Removed Test Report context

--- utilities/custom_logger.py
+++ utilities/custom_logger.py
@@ -4,7 +4,7 @@ class LogGen:
     @staticmethod
     def restful_booker_api_logs():
         formatter = logging.Formatter("%(asctime)s : %(levelname)s : %(message)s")
-        handler = logging.FileHandler(filename='.\\logs\\restful_booker_api.log')
+        handler = logging.FileHandler(filename=r"C:\Users\pfarakate\OneDrive - Infor\Frameworks\Demo_Framework\api-automation-framework-python\logs\restful_booker_api.log")
         handler.setFormatter(formatter)
 
         logger = logging.getLogger()
